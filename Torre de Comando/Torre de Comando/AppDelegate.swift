72d3fa991df37a818df8de51997cfae80279c2a9
matheusfalcao20 <aeromatheusfalcao@gmail.com>
2016-03-14 23:17:18 -0300

    Criação do projeto

---
+++ Torre de Comando/Torre de Comando/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Torre de Comando
+//
+//  Created by Matheus Falcão on 14/03/16.
+//  Copyright © 2016 Matheus Falcão. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Torre de Comando/Torre de Comando/Controllers/MainViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MainViewController.swift
+//  Torre de Comando
+//
+//  Created by Matheus Falcão on 14/03/16.
+//  Copyright © 2016 Matheus Falcão. All rights reserved.
+//
+
+import Cocoa
+
+class MainViewController: NSViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    override var representedObject: AnyObject? {
+        didSet {
+            // Update the view, if already loaded.
+        }
+    }
+    
+    
+}
+
